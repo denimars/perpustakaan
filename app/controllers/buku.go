7588f9ea90c8084e364a44cc1d5afb28f1e8f438
Deni Marswandi <denimarswandi@gmail.com>
2018-01-12 14:53:05 +0800

    navbar and route

--- app/controllers/buku.go
+++ app/controllers/buku.go
@@ -16,8 +16,6 @@ type Buku struct {
 func (c Buku) Index() revel.Result {
 	var buku []models.Buku
 	app.DB.Find(&buku)
-	log.Println("________________")
-	log.Println(buku)
 	return c.Render(buku)
 }
 
@@ -26,6 +24,10 @@ func (c Buku) Simpan(kdbuku string, judul string, pengarang string) revel.Result
 	buku.KdBuku = kdbuku
 	buku.Judul = judul
 	buku.Pengarang = pengarang
-	app.DB.Save(&buku)
+	err := app.DB.Save(&buku)
+	if err.Error != nil {
+		log.Println("________________")
+		panic(err.Error)
+	}
 	return c.Redirect(routes.Buku.Index())
 }
